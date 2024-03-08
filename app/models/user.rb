7d1da416d1d3fcc6300728abb5fb4cb1c2d70a73
EC2 Default User <ranm358m@gmail.com>
2024-03-08 13:55:30 +0000

    [Add]フォロー機能の解説を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,12 +8,26 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
   
-  # フォロー機能
+# フォロー機能
+#＝＝＝＝＝＝＝ 自分がフォローしているユーザーとの関連＝＝＝＝＝＝＝========================================
+  # follower =>フォローする側、followed =>フォローされる側
+  # 名前被りが起きないように、フォローする側のUserから見たrelationshipを「active_relationships」と定義
+  #フォローする側のUserから見て、フォローされる側のUserを(中間テーブル/relationshipsを介して)集める。
+  # なので親はfollower_id(フォローする側)
   has_many :active_relationships, class_name: "Relationship", foreign_key:"follower_id", dependent: :destroy
-  has_many :passive_relationships, class_name: "Relationship", foreign_key:"followed_id", dependent: :destroy
+  #中間テーブルを介して「followed」モデルのUser(フォローされた側/followed)を集めることを「followings」と定義
   has_many :followings, through: :active_relationships, source: :followed
-  has_many :followers, through: :passive_relationships, source: :follower
+# ===========================================================================================================
   
+#＝＝＝＝＝＝＝ 自分がフォローされるユーザーとの関連＝＝＝＝＝＝＝======================================
+  # follower =>フォローする側、followed =>フォローされる側
+  # 名前被りが起きないように、フォローされる側のUserから見たrelationshipを「passive_relationships」と定義
+  #フォローされる側のUserから見て、フォローする側のUserを(中間テーブル/relationshipsを介して)集める。
+  # なので親はfollowed_id(フォローされる側)
+  has_many :passive_relationships, class_name: "Relationship", foreign_key:"followed_id", dependent: :destroy
+  #中間テーブルを介して「follower」モデルのUser(フォローされた側/follower)を集めることを「followers」と定義
+  has_many :followers, through: :passive_relationships, source: :follower
+  # =========================================================================================================
 
   has_one_attached :profile_image
 
